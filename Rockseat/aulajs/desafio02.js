e2f1ee0b96fc3982659b98827711a7ac20f7fe76
LARA DOS SANTOS <laarasantos12@gmail.com>
2020-02-02 13:38:44 -0300

    Desafio 1 e 2, objetos e arrays

---
+++ Rockseat/aulajs/desafio02.js
@@ -0,0 +1,21 @@
+const usuario = {
+    nome: 'Carlos',
+    idade: 32,
+    tecnologia: [
+        { nome: 'C++', especialidade: 'Desktop' },
+        { nome: 'Python', especialidade: 'Data Science' },
+        { nome: 'JavaScript', especialidade: 'Web/Mobile'}
+    ],
+    empresa: {
+        nome: 'Rockseat',
+        cor: 'Roxo',
+        foco: 'Programação',
+        endereco: {
+            rua: 'Rua Guilherme Gembala',
+            num: '260'
+        }
+    }
+}
+
+console.log(`O usuário ${usuario.nome} tem ${usuario.idade} anos e usa a tecnologia ${usuario.tecnologia[0].nome} com especialidade em ${usuario.tecnologia[0].especialidade}`);
+console.log(`A empresa ${usuario.empresa.nome} está localizada em ${usuario.empresa.endereco.rua},${usuario.empresa.endereco.num}`);
